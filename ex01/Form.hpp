dacb865999c78db86b8e54a181d391b3835bd33a
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-07-26 11:36:54 +0200

    build: implement ex01

---
+++ ex01/Form.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klamprak <klamprak@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/26 11:13:03 by klamprak          #+#    #+#             */
+/*   Updated: 2024/07/26 11:32:44 by klamprak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat;
+
+class Form{
+	private:
+		const std::string _name;
+		const int _signed_grade;
+		const int _exec_grade;
+		bool _is_signed;
+
+	public:
+		// constructors
+		Form();
+		Form(const std::string name, const int singed_grade, const int exec_grade);
+		Form(const Form &other);
+		~Form();
+		Form &operator=(const Form &other);
+
+		// exceptions
+		class GradeTooHighException : public std::exception{
+			const char *what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception{
+			const char *what() const throw();
+		};
+
+		// Getters
+		bool isSigned() const;
+		std::string getName() const;
+		int getSignedGrade() const;
+		int getExecGrade() const;
+
+		// other
+		void beSigned(const Bureaucrat &b);
+};
+
+std::ostream &operator<<(std::ostream &os, const Form &f);
