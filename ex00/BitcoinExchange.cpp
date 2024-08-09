e87c4b7f50f50f42ca475299e2b77e50a8ff9a8a
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-08-09 17:01:46 +0200

    feat: implement the line processing, finish ex00

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 17:00:55 by klamprak          #+#    #+#             */
+/*   Updated: 2024/08/09 17:00:58 by klamprak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
@@ -46,13 +57,18 @@ void BitcoinExchange::getData(const std::string &file_name){
     }
 }
 
-void  BitcoinExchange::printValue(const std::string &date, const std::string &count_str) const{
-    std::cout << "=" << date << "=" << count_str << "=\n";
+void  BitcoinExchange::printValue(const std::string &date, const float &count) const{
+    std::string result = getClosestDateBefore(dataMap, date);
+    if (result == "NULL")
+        std::cout << "Not valid date prior to " << date << std::endl;
+    std::cout << date << " => " << count << " = " << dataMap.find(result)->second * count << std::endl;
 }
 
 void BitcoinExchange::proccessData(const std::string &file_name) const{
     std::ifstream file(file_name);
     std::string line;
+    std::regex pattern_date("^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$");
+    std::regex pattern_float("^(?:1000(?:\\.0*)?|(?:[0-9]|[1-9][0-9]|[1-9][0-9]{2})(?:\\.[0-9]*)?)$");
 
     if (file.is_open()) {
         std::getline(file, line); // skip header
@@ -65,7 +81,15 @@ void BitcoinExchange::proccessData(const std::string &file_name) const{
             std::getline(ss, count, '|');
             date = trim(date);
             count = trim(count);
-            printValue(date, count);
+            if (!std::regex_match(date, pattern_date)) {
+                std::cerr << "Error: not valid date : " << date << std::endl;
+                continue ;
+            }
+            if (!std::regex_match(count, pattern_float)) {
+                std::cerr << "Error: not valid float : " << count << std::endl;
+                continue ;
+            }
+            printValue(date, std::stof(count));
         }
         file.close();
     } else {
@@ -80,4 +104,43 @@ std::string trim(const std::string& str) {
     }
     size_t last = str.find_last_not_of(' ');
     return str.substr(first, (last - first + 1));
+}
+
+std::tm stringToDate(const std::string &dateStr) {
+    std::tm date = {};
+    std::istringstream ss(dateStr);
+    ss >> std::get_time(&date, "%Y-%m-%d");
+    return date;
+}
+
+std::string dateToString(const std::tm &date) {
+    std::ostringstream ss;
+    ss << std::put_time(&date, "%Y-%m-%d");
+    return ss.str();
+}
+
+std::string getClosestDateBefore(const std::map<std::string, float> &dataMap, const std::string &dateStr) {
+    std::tm targetDate = stringToDate(dateStr);
+    std::time_t targetTime = std::mktime(&targetDate);
+
+    std::map<std::string, float>::const_iterator it = dataMap.lower_bound(dateStr);
+
+    if (it != dataMap.end() && it->first == dateStr) {
+        return it->first;
+    }
+
+    if (it == dataMap.begin()) {
+        return ("NULL");
+    }
+
+    --it;
+
+    std::tm closestDate = stringToDate(it->first);
+    std::time_t closestTime = std::mktime(&closestDate);
+
+    if (closestTime < targetTime) {
+        return it->first;
+    } else {
+        return ("NULL");
+    }
 }
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -1,9 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 17:00:44 by klamprak          #+#    #+#             */
+/*   Updated: 2024/08/09 17:01:00 by klamprak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
+
+#include <regex>
+#include <string>
+
+#include <ctime>
+#include <limits>
+#include <iomanip>
+
 #include <map>
 
 class BitcoinExchange {
@@ -17,8 +36,11 @@ class BitcoinExchange {
         ~BitcoinExchange();
 
         void getData(const std::string &file_name);
-        void printValue(const std::string &date, const std::string &count_str) const;
+        void printValue(const std::string &date, const float &count) const;
         void proccessData(const std::string &file_name) const;
 };
 
 std::string trim(const std::string& str);
+std::string getClosestDateBefore(const std::map<std::string, float> &dataMap, const std::string &dateStr);
+std::string dateToString(const std::tm &date);
+std::tm stringToDate(const std::string &dateStr);
